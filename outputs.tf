32045510b5a6a1b1aa98c9557de8f411f745271d
bashfulrobot <dustin@bashfulrobot.com>
2025-01-01 17:22:19 -0800

    chore: Refactor output file configuration
    - Removed the `null_resource` block for `output_talosconfig`
    - Removed the `null_resource` block for `output_kubeconfig`
    - Removed the `command` argument for the `local-exec` provisioner inside `output_talosconfig`
    - Removed the `command` argument for the `local-exec` provisioner inside `output_kubeconfig`
    
    [outputs.tf]
    - Remove the `null_resource` block for `output_talosconfig`
    - Remove the `null_resource` block for `output_kubeconfig`
    - Remove the `command` argument for the `local-exec` provisioner inside `output_talosconfig`
    - Remove the `command` argument for the `local-exec` provisioner inside `output_kubeconfig`

--- outputs.tf
+++ outputs.tf
@@ -7,15 +7,3 @@ output "kubeconfig" {
   value     = talos_cluster_kubeconfig.this.kubeconfig_raw
   sensitive = true
 }
-
-resource "null_resource" "output_talosconfig" {
-  provisioner "local-exec" {
-    command = "mkdir -p config_files && terraform output -raw ${var.cluster_name}_talosconfig > talosconfig"
-  }
-}
-
-resource "null_resource" "output_kubeconfig" {
-  provisioner "local-exec" {
-    command = "mkdir -p config_files && terraform output -raw kubeconfig > ${var.cluster_name}_kubeconfig"
-  }
-}

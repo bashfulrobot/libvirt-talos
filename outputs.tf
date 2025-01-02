bfb4aaa81dd436d51a0f34c1abd5721a99384ca6
bashfulrobot <dustin@bashfulrobot.com>
2025-01-01 17:15:08 -0800

    docs: Add null_resource blocks for outputting configuration files
    - Added `null_resource` blocks to output the `talosconfig` and `kubeconfig` files
    - Updated the outputs.tf file to include the new `null_resource` blocks
    
    [outputs.tf]
    - Add a `null_resource` block for outputting the `talosconfig` file
    - Add a `null_resource` block for outputting the `kubeconfig` file

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,17 @@ output "kubeconfig" {
   value     = talos_cluster_kubeconfig.this.kubeconfig_raw
   sensitive = true
 }
+
+resource "null_resource" "output_talosconfig" {
+  provisioner "local-exec" {
+    command = "mkdir -p config_files && terraform output -raw ${var.cluster_name}_talosconfig > talosconfig"
+  }
+  depends_on = [output.talosconfig]
+}
+
+resource "null_resource" "output_kubeconfig" {
+  provisioner "local-exec" {
+    command = "mkdir -p config_files && terraform output -raw kubeconfig > ${var.cluster_name}_kubeconfig"
+  }
+  depends_on = [output.kubeconfig]
+}

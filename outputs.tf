6ca362b2079aeb993b660979a1b1d3943de62ea0
bashfulrobot <dustin@bashfulrobot.com>
2025-01-01 17:20:42 -0800

    chore: Remove dependencies on outputs.tf in null_resource
    - Remove dependencies on `output.talosconfig` and `output.kubeconfig` in `null_resource.output_talosconfig` and `null_resource.output_kubeconfig` in `outputs.tf` file.
    
    [outputs.tf]
    - Remove the dependency on `output.talosconfig` in `null_resource.output_talosconfig`
    - Remove the dependency on `output.kubeconfig` in `null_resource.output_kubeconfig`

--- outputs.tf
+++ outputs.tf
@@ -12,12 +12,10 @@ resource "null_resource" "output_talosconfig" {
   provisioner "local-exec" {
     command = "mkdir -p config_files && terraform output -raw ${var.cluster_name}_talosconfig > talosconfig"
   }
-  depends_on = [output.talosconfig]
 }
 
 resource "null_resource" "output_kubeconfig" {
   provisioner "local-exec" {
     command = "mkdir -p config_files && terraform output -raw kubeconfig > ${var.cluster_name}_kubeconfig"
   }
-  depends_on = [output.kubeconfig]
 }

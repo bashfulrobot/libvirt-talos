1bd25ab7f9aaf607748322cbf77d1e8d376832d1
bashfulrobot <dustin@bashfulrobot.com>
2025-01-10 11:50:01 -0800

    chore: Add new Terraform module for Cilium deployment and remove unnecessary block
    - Added a new Terraform module for Cilium deployment
    - Set the count to 1 if `cilium_enable` is true, else set it to 0
    - Set various variables for Cilium deployment
    - Added dependency on `talos_machine_bootstrap.this` resource
    - Removed `module "cilium"` block from `talos.tf`
    
    [cilium.tf]
    - Add a new Terraform module for Cilium deployment
    - Set the count to 1 if the variable `cilium_enable` is true, else set it to 0
    - Set various variables for Cilium deployment, such as `kvm_subnet`, `kvm_cidr`, `cilium_version`, `kubernetes_version`, `cilium_lb_first_ip`, and `cilium_lb_last_ip`
    - Add a dependency on the `talos_machine_bootstrap.this` resource
    [talos.tf]
    - Remove the `module "cilium"` block from `talos.tf`

--- talos.tf
+++ talos.tf
@@ -63,16 +63,3 @@ resource "talos_cluster_kubeconfig" "this" {
   endpoint             = local.controller_nodes[0].address
   node                 = local.controller_nodes[0].address
 }
-
-module "cilium" {
-  source             = "./modules/cilium"
-  count              = var.cilium_enable ? 1 : 0
-  kvm_subnet         = var.kvm_subnet
-  kvm_cidr           = var.kvm_cidr
-  cilium_version     = var.cilium_version
-  kubernetes_version = var.kubernetes_version
-  cilium_lb_first_ip = var.cilium_lb_first_ip
-  cilium_lb_last_ip  = var.cilium_lb_last_ip
-
-  depends_on = [talos_machine_bootstrap.this]
-}
